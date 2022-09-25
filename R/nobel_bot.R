9d54ffd10423d1ecf7bc9617ebafe53052b6d7b9
jbengler <broder.engler@gmail.com>
2022-09-25 13:30:27 +0200

    workflow

--- R/nobel_bot.R
+++ R/nobel_bot.R
@@ -1,4 +1,5 @@
 
+
 # this is needed to allow special characters in names in Rscript execution
 # Sys.setlocale(category = "LC_CTYPE", locale = "de_DE.UTF-8")
 
@@ -10,24 +11,24 @@ library(stringi)
 
 nobel_winners <- read_csv("nobel_winners.csv")
 
-win <- 
-  nobel_winners %>% 
-  filter(category == "Medicine") %>% 
-  distinct(full_name, prize, motivation) %>% 
-  group_by(prize, motivation) %>% 
+win <-
+  nobel_winners %>%
+  filter(category == "Medicine") %>%
+  distinct(full_name, prize, motivation) %>%
+  group_by(prize, motivation) %>%
   summarise(
     names = paste(full_name, collapse = "*, *"),
     prize = unique(prize),
     motivation = unique(motivation)
-  ) %>% 
-  ungroup() %>% 
+  ) %>%
+  ungroup() %>%
   mutate(
     names = paste0("*",names,"*"),
     names = stri_replace_last_fixed(names, "*, *", "* and *")
-  ) %>% 
+  ) %>%
   slice_sample(n = 1)
 
-message <- paste0(win$names, 
+message <- paste0(win$names,
 " received _",win$prize,"_ ",str_replace_all(win$motivation, "\"", ""),".")
 cmd <- paste0("curl -X POST -H 'Content-type: application/json' --data '{\"text\":\"", message ,"\"}' ",
               Sys.getenv("INIMS_SLACK_TEST_HOOK"))
