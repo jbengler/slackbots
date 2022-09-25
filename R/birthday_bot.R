bfb084354b98287f5b404b33dd560ab590b10b37
jbengler <broder.engler@gmail.com>
2022-09-25 02:44:49 +0200

    Add GitHub links to DESCRIPTION

---
+++ R/birthday_bot.R
@@ -0,0 +1,24 @@
+
+# this is needed to allow special characters in names in Rscript execution
+# Sys.setlocale(category = "LC_CTYPE", locale = "de_DE.UTF-8")
+
+library(tidyverse)
+library(googlesheets4)
+
+googlesheets4::gs4_auth(email = Sys.getenv("INIMS_SHEETS_GOOGLE_MAIL"), path = "inims_sheets.json")
+
+df <- read_sheet(Sys.getenv("INIMS_SHEETS_BIRTHDAYS"))
+
+today <- paste0(as.numeric(format(Sys.Date(), "%m")),"_",as.numeric(format(Sys.Date(), "%d")))
+
+name <- 
+  df %>% 
+  filter(Date == today) %>% 
+  pull(Name)
+
+for (i in name) {
+  message <- paste0("Today is the birthday of *",i,"* :tada:")
+  cmd <- paste0("curl -X POST -H 'Content-type: application/json' --data '{\"text\":\"", message ,"\"}' ",
+                Sys.getenv("INIMS_SLACK_TEST_HOOK"))
+  cmd %>% system
+}
---
+++ R/nobel_bot.R
@@ -0,0 +1,35 @@
+
+# this is needed to allow special characters in names in Rscript execution
+# Sys.setlocale(category = "LC_CTYPE", locale = "de_DE.UTF-8")
+
+library(tidyverse)
+library(stringi)
+
+# nobel_winners <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-14/nobel_winners.csv")
+# nobel_winner_all_pubs <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-14/nobel_winner_all_pubs.csv")
+
+nobel_winners <- read_csv("nobel_winners.csv")
+
+win <- 
+  nobel_winners %>% 
+  filter(category == "Medicine") %>% 
+  distinct(full_name, prize, motivation) %>% 
+  group_by(prize, motivation) %>% 
+  summarise(
+    names = paste(full_name, collapse = "*, *"),
+    prize = unique(prize),
+    motivation = unique(motivation)
+  ) %>% 
+  ungroup() %>% 
+  mutate(
+    names = paste0("*",names,"*"),
+    names = stri_replace_last_fixed(names, "*, *", "* and *")
+  ) %>% 
+  slice_sample(n = 1)
+
+message <- paste0(win$names, 
+" received _",win$prize,"_ ",str_replace_all(win$motivation, "\"", ""),".")
+cmd <- paste0("curl -X POST -H 'Content-type: application/json' --data '{\"text\":\"", message ,"\"}' ",
+              Sys.getenv("INIMS_SLACK_TEST_HOOK"))
+cmd
+cmd %>% system
