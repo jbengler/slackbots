b41d8c043ffda798a1ba2794d4b434030ed967e4
jbengler <broder.engler@gmail.com>
2022-11-19 09:54:08 +0100

    keep jobs active

--- R/run_cron.R
+++ R/run_cron.R
@@ -1,12 +1,13 @@
 
+
 # library(cronR)
-# 
+#
 # cmd <-
-#   cron_rscript("nobel_bot.R") %>% 
+#   cron_rscript("nobel_bot.R") %>%
 #   str_replace("Rscript", "Rscript --encoding=UTF-8")
-# 
+#
 # cron_add(cmd, frequency = '*/1 * * * *', id = 'nobel_bot', description = 'Every 1 min')
-# 
+#
 # cron_ls()
 # cron_clear(ask = TRUE)
 # cron_ls()
